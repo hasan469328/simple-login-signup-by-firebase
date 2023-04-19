2035987255c690058c70d232ae0152cefa8721a3
hasan469328 <hasanur.68.bd@gmail.com>
2023-04-19 20:57:28 +0600

    added signout option and conditionally set the login button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProviders";
 
 const Header = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+    .then(() => {
+
+    })
+    .catch(error => {
+      console.error(error.message)
+    })
+  }
+
   return (
     <div className="navbar bg-primary text-primary-content flex justify-between items-center">
       <Link to="/" className="btn btn-ghost normal-case text-xl">
@@ -11,12 +24,24 @@ const Header = () => {
         <Link className="btn btn-ghost normal-case text-xl" to="/">
           Home
         </Link>
-        <Link className="btn btn-ghost normal-case text-xl" to="/login">
+        {/* <Link className="btn btn-ghost normal-case text-xl" to="/login">
           Login
-        </Link>
+        </Link> */}
         <Link className="btn btn-ghost normal-case text-xl" to="/register">
           Register
         </Link>
+        {user ? (
+          <>
+            <span>{user.email}</span> &nbsp;
+            <a onClick={handleLogOut} className="btn btn-xs normal-case" href="">
+              Log Out
+            </a>
+          </>
+        ) : (
+          <Link className="btn btn-ghost normal-case text-xl" to="/login">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../Firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -18,10 +18,25 @@ const AuthProviders = ({children}) => {
     return signInWithEmailAndPassword(auth, email, password)
   }
 
+  const logOut = () => {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      console.log('on state change', currentUser)
+    });
+    return () => {
+      unsubscribe()
+    } 
+  }, [])
+
   const authInfo = {
     user,
     signUp,
-    signIn
+    signIn,
+    logOut
   }
 
   return (
